1e50ea9397772b2c4d780b683c2ef15e4bbe4b8f
Faruk Imamovic <farukt9@gmail.com>
2025-05-01 20:31:32 +0200

    fixed proxmox ip

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,7 +9,7 @@ terraform {
 }
 
 provider "proxmox" {
-  pm_api_url          = "https://192.168.178.10:8006/api2/json"
+  pm_api_url          = "https://192.168.178.200:8006/api2/json"
   pm_api_token_id     = var.pm_api_token_id
   pm_api_token_secret = var.pm_api_token_secret
   pm_tls_insecure     = true
